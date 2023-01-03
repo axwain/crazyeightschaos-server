b9a0d8963885a9c4eedb912f4e3d749226d2ed2b
AXwain <5733365+axwain@users.noreply.github.com>
2023-01-02 22:36:44 -0500

    * Moved Card enums to their own file * Fixed typo on Card Suit property

--- PlayLib/Entities/Card.cs
+++ PlayLib/Entities/Card.cs
@@ -1,34 +1,17 @@
+using CrazyEights.PlayLib.Enums;
+
 namespace CrazyEights.PlayLib.Entities
 {
-    public enum Suits
-    {
-        Wild = 0,
-        Spade,
-        Club,
-        Heart,
-        Diamond,
-        Star
-    }
-
-    public enum Effects
-    {
-        None = 0,
-        DrawCards,
-        SkipTurn,
-        ReverseTurnOrder,
-        InterchangeHands
-    }
-
     public class Card
     {
-        public Suits SuiteId { get; private set; }
+        public Suits SuitId { get; private set; }
         public int Value { get; private set; }
         public Effects EffectId { get; private set; }
         public int EffectArg { get; private set; }
 
         public Card(Suits suit, int value, Effects effect, int effectArg)
         {
-            SuiteId = suit;
+            SuitId = suit;
             Value = value;
             EffectId = effect;
             EffectArg = effectArg;
@@ -36,7 +19,7 @@ namespace CrazyEights.PlayLib.Entities
 
         public bool Equals(Card card)
         {
-            return SuiteId == card.SuiteId && Value == card.Value;
+            return SuitId == card.SuitId && Value == card.Value;
         }
     }
 }
---
+++ PlayLib/Enums/CardEnums.cs
@@ -0,0 +1,21 @@
+namespace CrazyEights.PlayLib.Enums
+{
+    public enum Suits
+    {
+        Star = 0,
+        Spade,
+        Club,
+        Heart,
+        Diamond,
+        Wild
+    }
+
+    public enum Effects
+    {
+        None = 0,
+        DrawCards,
+        SkipTurn,
+        ReverseTurnOrder,
+        InterchangeHands
+    }
+}
