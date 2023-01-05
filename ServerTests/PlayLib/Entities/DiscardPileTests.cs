a1df8a306b3eedbdfd62fa4423b70ba99ed84b05
AXwain <5733365+axwain@users.noreply.github.com>
2023-01-04 22:18:28 -0500

    Removed TopCard from DiscardPile

--- ServerTests/PlayLib/Entities/DiscardPileTests.cs
+++ ServerTests/PlayLib/Entities/DiscardPileTests.cs
@@ -12,8 +12,6 @@ namespace CrazyEights.Tests.PlayLib.Entities
             var cardCount = 1;
             var firstCard = new Card(Suits.Spade, 1, Effects.None, 0);
             var discardPile = new DiscardPile(firstCard, cardCount);
-            discardPile.TopCard.Update(firstCard.SuitId, firstCard.Value);
-            Assert.That(discardPile.TopCard.Matches(firstCard), Is.True, "Pile Top Card matches the first card");
             Assert.That(discardPile.IsEmpty, Is.False, "Pile is not empty after initialization");
         }
 
@@ -29,119 +27,26 @@ namespace CrazyEights.Tests.PlayLib.Entities
             );
         }
 
-        [Test, Description("Check if card can be added to pile")]
-        public void DiscardPile_CanAdd_Card()
-        {
-            var cardCount = 1;
-            var firstCard = new Card(Suits.Spade, 1, Effects.None, 0);
-            var wildCard = new Card(Suits.Wild, 0, Effects.None, 0);
-            var sameSuitCard = new Card(Suits.Spade, 5, Effects.None, 0);
-            var sameValueCard = new Card(Suits.Star, 1, Effects.None, 0);
-            var differentCardOne = new Card(Suits.Club, 5, Effects.None, 0);
-            var differentCardTwo = new Card(Suits.Diamond, 2, Effects.None, 0);
-            var differentCardThree = new Card(Suits.Heart, 3, Effects.SkipTurn, 0);
-            var discardPile = new DiscardPile(firstCard, cardCount);
-            discardPile.TopCard.Update(firstCard.SuitId, firstCard.Value);
-
-            Assert.That(discardPile.TopCard.Matches(wildCard), Is.True, "Can add wild card");
-            Assert.That(discardPile.TopCard.Matches(sameSuitCard), Is.True, "Can add card with same suit");
-            Assert.That(discardPile.TopCard.Matches(sameValueCard), Is.True, "Can add card with same value");
-            Assert.That(
-                discardPile.TopCard.Matches(differentCardOne),
-                Is.False,
-                "Can't that's not a wild, with same suit, or with same value"
-            );
-            Assert.That(
-                discardPile.TopCard.Matches(differentCardTwo),
-                Is.False,
-                "Can't that's not a wild, with same suit, or with same value"
-            );
-            Assert.That(
-                discardPile.TopCard.Matches(differentCardThree),
-                Is.False,
-                "Can't that's not a wild, with same suit, or with same value"
-            );
-        }
-
+        [TestCase(Suits.Club, 1, Effects.DrawCards, 1)]
+        [TestCase(Suits.Diamond, 2, Effects.InterchangeHands, -1)]
+        [TestCase(Suits.Heart, 3, Effects.None, 0)]
+        [TestCase(Suits.Spade, 4, Effects.ReverseTurnOrder, -1)]
+        [TestCase(Suits.Star, 5, Effects.SkipTurn, 1)]
+        [TestCase(Suits.Wild, 0, Effects.DrawCards, 4)]
         [Test, Description("Add Card to the pile")]
-        public void DiscardPile_Add_Card()
+        public void DiscardPile_Add_Card(Suits suit, int value, Effects effect, int args)
         {
-            var cardCount = 6;
+            var maxCardCount = 2;
             var firstCard = new Card(Suits.Spade, 1, Effects.None, 0);
-            var wildCard = new Card(Suits.Wild, 0, Effects.None, 0);
-            var cardAfterWild = new Card(Suits.Club, 3, Effects.None, 0);
-            var sameSuitCard = new Card(Suits.Club, 5, Effects.None, 0);
-            var sameValueCard = new Card(Suits.Star, 5, Effects.None, 0);
-            var discardPile = new DiscardPile(firstCard, cardCount);
+            var card = new Card(suit, value, effect, args);
+            var discardPile = new DiscardPile(firstCard, maxCardCount);
 
-            discardPile.TopCard.Update(firstCard.SuitId, firstCard.Value);
-            discardPile.Add(wildCard);
+            discardPile.Add(card);
+            var cards = discardPile.Clear();
             Assert.That(
-                discardPile.TopCard.Matches(wildCard),
-                Is.True,
-                "Wild card was added at the top of the pile"
-            );
-
-            discardPile.TopCard.Update(Suits.Diamond, -1);
-            discardPile.Add(wildCard);
-            Assert.That(
-                discardPile.TopCard.Matches(wildCard),
-                Is.True,
-                "A wild card after a wild card was added at the top of the pile"
-            );
-
-            discardPile.TopCard.Update(cardAfterWild.SuitId, -1);
-            discardPile.Add(cardAfterWild);
-            Assert.That(
-                discardPile.TopCard.Matches(cardAfterWild),
-                Is.True,
-                "A card after a wild was added at the top of the pile"
-            );
-
-            discardPile.TopCard.Update(cardAfterWild.SuitId, cardAfterWild.Value);
-            discardPile.Add(sameSuitCard);
-            Assert.That(
-                discardPile.TopCard.Matches(sameSuitCard),
-                Is.True,
-                "A card with the same suit was added at the top of the pile"
-            );
-
-            discardPile.TopCard.Update(sameSuitCard.SuitId, sameSuitCard.Value);
-            discardPile.Add(sameValueCard);
-            Assert.That(
-                discardPile.TopCard.Matches(sameValueCard),
-                Is.True,
-                "A card with the same value was added at the top of the pile"
-            );
-        }
-
-        [Test, Description("Can't Add An Invalid Card to the pile")]
-        public void DiscardPile_Add_Invalid_Card()
-        {
-            var cardCount = 1;
-            var firstCard = new Card(Suits.Spade, 1, Effects.None, 0);
-            var differentCardOne = new Card(Suits.Club, 2, Effects.None, 0);
-            var differentCardTwo = new Card(Suits.Diamond, 3, Effects.None, 0);
-            var differentCardThree = new Card(Suits.Heart, 4, Effects.None, 0);
-            var discardPile = new DiscardPile(firstCard, cardCount);
-
-            discardPile.TopCard.Update(firstCard.SuitId, firstCard.Value);
-            Assert.That(
-                () => discardPile.Add(differentCardOne),
-                Throws.TypeOf<ArgumentException>().With.Property("ParamName").EqualTo("card"),
-                "Discard Pile must be initialized with a card count greater than zero"
-            );
-
-            Assert.That(
-                () => discardPile.Add(differentCardTwo),
-                Throws.TypeOf<ArgumentException>().With.Property("ParamName").EqualTo("card"),
-                "Discard Pile must be initialized with a card count greater than zero"
-            );
-
-            Assert.That(
-                () => discardPile.Add(differentCardThree),
-                Throws.TypeOf<ArgumentException>().With.Property("ParamName").EqualTo("card"),
-                "Discard Pile must be initialized with a card count greater than zero"
+                cards.Length,
+                Is.EqualTo(2),
+                $"should have added {suit} {value} to the discard pile"
             );
         }
 
@@ -154,26 +59,18 @@ namespace CrazyEights.Tests.PlayLib.Entities
             var lastCard = new Card(Suits.Club, 3, Effects.None, 0);
             var discardPile = new DiscardPile(firstCard, cardCount);
 
-            discardPile.TopCard.Update(firstCard.SuitId, firstCard.Value);
             discardPile.Add(wildCard);
-            discardPile.TopCard.Update(lastCard.SuitId, -1);
             discardPile.Add(lastCard);
-            discardPile.TopCard.Update(lastCard.SuitId, lastCard.Value);
 
-            var cards = discardPile.Clear();
+            var pile = discardPile.Clear();
+            Card[] cards = { firstCard, wildCard, lastCard };
 
             Assert.That(discardPile.IsEmpty, Is.True, "Pile is empty after clearing");
-            Assert.That(cards.Length, Is.EqualTo(cardCount), "Pile had three cards");
-            Assert.That(
-                firstCard.Equals(cards[0]),
-                Is.True,
-                "The first discarded card is the first returned card"
-            );
-            Assert.That(
-                lastCard.Equals(cards[2]),
-                Is.True,
-                "The last discarded card is the last returned card"
-            );
+            Assert.That(pile.Length, Is.EqualTo(cardCount), "Pile had three cards");
+            for (var i = 0; i < cardCount; i++)
+            {
+                Assert.That(cards[i].Equals(pile[i]), Is.True, $"should had {pile[i].SuitId} {pile[i].Value} in the pile");
+            }
         }
     }
 }
