a53a86df82f645766a33801b4810e8e66b6c3059
AXwain <5733365+axwain@users.noreply.github.com>
2023-01-08 22:55:58 -0500

    Implemented RuleSet entity
    * Defined initial rule ids enum
    * Defined RuleSet entity to hold the values of the rules
    * Added test for the RuleSet entity

---
+++ PlayLib/Enums/RuleSetEnums.cs
@@ -0,0 +1,13 @@
+namespace CrazyEights.PlayLib.Enums
+{
+    public enum RuleIds
+    {
+        AceCardEffect,
+        WCardEffect,
+        TurnJump,
+        StackDrawEffect,
+        WildDrawOverSuitDraw,
+        WildFinish,
+        DrawToMatch
+    }
+}
